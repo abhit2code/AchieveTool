45d5189f4432cb94f2f2861a44733667bb33f512
abhit2code <abhit.tech@gmail.com>
2024-04-05 11:14:28 +0530

    feat(Chat): OpenAI api call

--- client/src/components/AchieveTool.js
+++ client/src/components/AchieveTool.js
@@ -3,8 +3,45 @@ import { IconButton } from "@mui/material";
 import LightbulbIcon from "@mui/icons-material/Lightbulb";
 import NotificationImportantIcon from "@mui/icons-material/NotificationImportant";
 import "./AchieveTool.css";
+import axios from "axios";
 
 const AchieveTool = () => {
+  const inputText = "Hello I need help";
+  const openAiApiKey = "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";
+  const openAiApiUrl = "https://api.openai.com/v1/completions";
+
+  console.log("OpenAI API Key:", openAiApiKey);
+
+  const makeApiCall = async () => {
+    console.log("Making API call to OpenAI");
+    try {
+      const response = await axios
+        .post(
+          openAiApiUrl,
+          {
+            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: inputText }],
+          },
+          {
+            headers: {
+              Authorization: `Bearer ${openAiApiKey}`,
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((response) => {
+          const output = response.data.choices[0].text.trim();
+          console.log("OpenAI response:", output);
+        })
+        .catch((error) => {
+          console.error("Error calling OpenAI API:", error);
+        });
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div
       style={{
@@ -22,6 +59,7 @@ const AchieveTool = () => {
       <NotificationImportantIcon
         className="changingSentenceNeutral"
         style={{ fontSize: "2rem" }}
+        onClick={makeApiCall}
       />
       <LightbulbIcon className="suggestion" style={{ fontSize: "2rem" }} />
     </div>
