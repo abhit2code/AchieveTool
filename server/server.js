b2d3f9c32723f249cd6a95cead69ea132cfc1bb6
abhit2code <abhit.tech@gmail.com>
2024-05-23 17:11:14 +0530

    trying new way of allow origin

--- server/server.js
+++ server/server.js
@@ -8,7 +8,16 @@ import { HarmBlockThreshold, HarmCategory } from "@google/generative-ai";
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type,  Authorization");
+  // res.header("Access-Control-Allow-Credentials", true);
+  // res.header("Access-Control-Expose-Headers", "set-cookie");
+  next();
+});
 
 let userNames = [];
 let freeSocketIds = [];
