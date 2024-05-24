ecaadff6cd282c5c545b8b5a5f1f31ca47d92aee
abhit2code <abhit.tech@gmail.com>
2024-05-24 17:40:53 +0530

    resolved https error

--- client/src/ChatSocket.js
+++ client/src/ChatSocket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 // const socket = io(process.env.REACT_APP_SERVER_URL);
 const socket = io("https://froot-achievetool.onrender.com", {
-  transports: ["websocket"], // Use WebSocket transport
+  transports: ["websocket", "polling"], // Use WebSocket transport
   withCredentials: true, // Send cookies if needed
 });
 
